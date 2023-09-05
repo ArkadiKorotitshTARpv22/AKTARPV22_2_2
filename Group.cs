590de442e67a4e143ddb32000e62bf5dbabef300
opilane <opilane@HP-CZC2349HT9>
2023-09-05 16:23:09 +0300

    Add project files.

---
+++ Group.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public class Group
+    {
+        public List<string> Members { get; } = new List<string>();
+        private readonly int _maxAmount;
+
+        public Group(int maxAmount)
+        {
+            _maxAmount = maxAmount;
+        }
+    public string PeopleList() 
+    { 
+        Random random= new Random();
+        var peole = new List<string> { "Josh","John","Joseph","Jonathan","Arkadi","Luca","Alex","Kris","Jura","Artur","Egor","Anja","Aleksander","Oleksander","Dmitriy","Irina"};
+        int chosenPerson = random.Next(peole.Count);
+        return peole[chosenPerson];
+    }
+        public bool AddMember(string member)
+        {
+            if (Members.Contains(member)) return false;
+            if (Members.Count >= _maxAmount) return false;
+            Members.Add(member);
+            return true;
+        }
+
+        public int GetMembersCount()
+        {
+            return Members.Count;
+        }
+
+        public bool HasMember(string member)
+        {
+            return Members.Contains(member);
+        }
+
+    
+
+    
+    }
+
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        Random random = new Random();
+        Console.WriteLine("kui palju inimesi soovite grupiga liituda max:");
+        int m = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("grupi nimi:");
+        string n = Console.ReadLine();
+        Group group = new Group(m);
+        for (int i=0;i< random.Next(1, 1000); i++)
+        {  
+            Console.WriteLine(group.AddMember(group.PeopleList()));
+        }
+        Console.WriteLine("teie gruppi {0} kuuluvad:", n);
+        Console.WriteLine(string.Join(", ", group.Members));
+        Console.WriteLine(group.GetMembersCount());
+        Console.WriteLine("-------------------------");
+
+        Console.WriteLine("kui palju inimesi soovite grupiga liituda max:");
+        int m2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("grupi nimi:");
+        string n2 = Console.ReadLine();
+        Group group2 = new Group(m2);
+        Console.WriteLine("kellega soovite liituda:");
+        for (int i = 0; i < random.Next(1, 1000); i++)
+        {
+            Console.WriteLine(group.AddMember(group.PeopleList()));
+        }
+
+    }
+}

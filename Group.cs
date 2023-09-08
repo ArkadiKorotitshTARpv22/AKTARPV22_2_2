109b5c3ac5d18df1a3d19ae4c1feb6b4343b4a9b
Arkadi Korotots <arkadi.korotots@mjg.ee>
2023-09-08 10:57:14 +0300

    a

--- Group.cs
+++ Group.cs
@@ -2,43 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading.Tasks;//"Josh","John","Joseph","Jonathan","Arkadi","Luca","Alex","Kris","Jura","Artur","Egor","Anja","Aleksander","Oleksander","Dmitriy","Irina"//
 public class Group
+{
+    public List<string> Members { get; } = new List<string>();
+    private readonly int _maxAmount;
+    private Dictionary<string, int> _membersWithAge = new Dictionary<string, int>();
+
+    public Group(int maxAmount)
     {
-        public List<string> Members { get; } = new List<string>();
-        private readonly int _maxAmount;
-
-        public Group(int maxAmount)
-        {
-            _maxAmount = maxAmount;
-        }
-    public string PeopleList() 
-    { 
-        Random random= new Random();
-        var peole = new List<string> { "Josh","John","Joseph","Jonathan","Arkadi","Luca","Alex","Kris","Jura","Artur","Egor","Anja","Aleksander","Oleksander","Dmitriy","Irina"};
-        int chosenPerson = random.Next(peole.Count);
-        return peole[chosenPerson];
+        _maxAmount = maxAmount;
     }
-        public bool AddMember(string member)
-        {
-            if (Members.Contains(member)) return false;
-            if (Members.Count >= _maxAmount) return false;
-            Members.Add(member);
-            return true;
-        }
 
-        public int GetMembersCount()
-        {
-            return Members.Count;
-        }
+    public bool AddMember(string member, int age)
+    {
+        if (Members.Contains(member) || Members.Count >= _maxAmount)
+            return false;
 
-        public bool HasMember(string member)
-        {
-            return Members.Contains(member);
-        }
-
-    
-
-    
+        Members.Add(member);
+        _membersWithAge[member] = age;
+        return true;
     }
+
+    public int GetMembersCount()
+    {
+        return Members.Count;
+    }
+
+    public bool HasMember(string member)
+    {
+        return Members.Contains(member);
+    }
+
+   
+}
 
--- Program.cs
+++ Program.cs
@@ -3,30 +3,99 @@
     public static void Main()
     {
         Random random = new Random();
-        Console.WriteLine("kui palju inimesi soovite grupiga liituda max:");
-        int m = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("grupi nimi:");
-        string n = Console.ReadLine();
-        Group group = new Group(m);
-        for (int i=0;i< random.Next(1, 1000); i++)
-        {  
-            Console.WriteLine(group.AddMember(group.PeopleList()));
-        }
-        Console.WriteLine("teie gruppi {0} kuuluvad:", n);
-        Console.WriteLine(string.Join(", ", group.Members));
-        Console.WriteLine(group.GetMembersCount());
-        Console.WriteLine("-------------------------");
 
-        Console.WriteLine("kui palju inimesi soovite grupiga liituda max:");
-        int m2 = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("grupi nimi:");
-        string n2 = Console.ReadLine();
-        Group group2 = new Group(m2);
-        Console.WriteLine("kellega soovite liituda:");
-        for (int i = 0; i < random.Next(1, 1000); i++)
+        List<string> group1Members = new List<string>();
+        List<string> group2Members = new List<string>();
+
+        for (int i = 0; i < 10; i++)
         {
-            Console.WriteLine(group.AddMember(group.PeopleList()));
+            string name1 = RandomName(random);
+            string name2 = RandomName(random);
+            group1Members.Add(name1);
+            group2Members.Add(name2);
         }
 
+        
+        Console.WriteLine("Group 1 inimesed, kes soovivad liituda:");
+        PrintNames(group1Members);
+
+        
+        Console.WriteLine("\nGroup 2 inimesed, kes soovivad liituda:");
+        PrintNames(group2Members);
+        
+
+        string groupName1 = RandomName(random);
+        Console.WriteLine("\nmilline on maksimaalne:");
+        int m = Convert.ToInt32(Console.ReadLine());
+        int maxAmount1 = m;
+        
+        Group group1 = new Group(maxAmount1);
+        Console.WriteLine($"\nGroup 1 maksimaalsed ruumid: {maxAmount1} ");
+
+        string groupName2 = RandomName(random);
+        Console.WriteLine("\nmilline on maksimaalne:");
+        int m2 = Convert.ToInt32(Console.ReadLine());
+        int maxAmount2 = m2;
+
+        Group group2 = new Group(maxAmount2);
+        Console.WriteLine($"Group 2 maksimaalsed ruumid: {maxAmount2}\n");
+        
+
+        for (int i = 0; i < maxAmount1; i++)
+        {
+            if (group1Members.Count > 0)
+            {
+                string memberName = group1Members[0];
+                int age = random.Next(16, 60);
+                group1.AddMember(memberName, age);
+                group1Members.RemoveAt(0);
+            }
+        }
+
+        for (int i = 0; i < maxAmount2; i++)
+        {
+            if (group2Members.Count > 0)
+            {
+                int randomIndex = random.Next(group2Members.Count);
+                string memberName = group2Members[randomIndex];
+                int age = random.Next(16, 60);
+                group2.AddMember(memberName, age);
+                group2Members.RemoveAt(randomIndex);
+            }
+        }
+        
+        Console.WriteLine($"Group 1 Successfully joined members: {string.Join(", ", group1.Members)}");
+        Console.WriteLine($"Group 2 Successfully joined members: {string.Join(", ", group2.Members)}  \n");
+
+        
+    }
+
+    public static void Shuffle<T>(IList<T> list, Random random)
+    {
+        int n = list.Count;
+        for (int i = 0; i < n; i++)
+        {
+            int r = i + random.Next(n - i);
+            T temp = list[i];
+            list[i] = list[r];
+            list[r] = temp;
+        }
+    }   
+    public static void PrintNames(List<string> names)
+    {
+        HashSet<string> uniqueNames = new HashSet<string>();
+        foreach (string name in names)
+        {
+            
+            Console.Write($"{name}, ");
+        }
+        
+        Console.WriteLine();
+    }
+
+    public static string RandomName(Random random)
+    {
+        string[] names = { "Josh", "John", "Joseph", "Jonathan", "Arkadi", "Luca", "Alex", "Kris", "Jura", "Artur", "Egor", "Anja", "Aleksander", "Oleksander", "Dmitriy", "Irina" };
+        return names[random.Next(names.Length)];
     }
 }
